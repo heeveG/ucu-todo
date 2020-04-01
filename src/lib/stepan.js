a93b4eb6599acbb793b5c8bfb5799aa5568d1e6e
heeve <nazarpasternak2001@gmail.com>
2020-04-01 22:52:44 +0300

    task done

--- src/lib/stepan.js
+++ src/lib/stepan.js
@@ -1,36 +1,52 @@
-
 export default class Stepan {
-  static createElement(element, parent, attributes = {}) {
-    // TODO: check if this is a valid tag name
-    const newElement = document.createElement(element);
+    static createElement(element, parent, attributes = {}) {
+        // TODO: check if this is a valid tag name
 
-    const { innerHTML, innerText } = attributes;
+        const newElement = document.createElement(element);
 
-    for (let attribute in attributes) {
-      if (['innerHTML', 'innerText'].includes(attribute)) {
-        continue;
-      }
+        if (newElement.toString() === "[object HTMLUnknownElement]") {
+            throw new StepanError("Tag name is invalid");
+        }
+        const {innerHTML, innerText, checked} = attributes;
 
-      newElement.setAttribute(attribute, attributes[attribute]);
+        for (let attribute in attributes) {
+            if (['innerHTML', 'innerText', 'checked'].includes(attribute)) {
+                continue;
+            }
+
+            newElement.setAttribute(attribute, attributes[attribute]);
+        }
+
+        innerHTML && (newElement.innerHTML = innerHTML);
+        innerText && (newElement.innerText = innerText);
+        checked && (newElement.checked = checked);
+
+        parent.appendChild(newElement);
+
+        return newElement;
     }
 
-    innerHTML && (newElement.innerHTML = innerHTML);
-    innerText && (newElement.innerText = innerText);
+    static Component = class {
+        constructor(parent) {
+            // TODO: 1. Create StepanError class to define all framework errors
+            //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
+            if (parent == null) {
+                throw new StepanError("Parent tag is null");
+            } else if (parent.toString() === "[object HTMLUnknownElement]") {
+                throw new StepanError("Tag name is invalid");
 
-    parent.appendChild(newElement);
+            } else {
+                this.parent = parent;
+            }
+        }
 
-    return newElement;
-  }
-
-  static Component = class {
-    constructor(parent) {
-
-      // TODO: 1. Create StepanError class to define all framework errors
-      //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
-
-      this.parent = parent;
+        // TODO (Bonus): Ensure that every component returns a top-level root element
     }
+}
 
-    // TODO (Bonus): Ensure that every component returns a top-level root element
-  }
+class StepanError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "StepanError";
+    }
 }
